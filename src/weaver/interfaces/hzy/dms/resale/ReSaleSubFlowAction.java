d7bf97206763a48ddde29606b2e9ab75e23296c1
justin-hzy <771736395@qq.com>
2024-07-12 10:19:14 +0800

    1.退货子流程拆当，增加将触发的子流程requestid保存到主流程中

--- src/weaver/interfaces/hzy/dms/resale/ReSaleSubFlowAction.java
+++ src/weaver/interfaces/hzy/dms/resale/ReSaleSubFlowAction.java
@@ -1,4 +1,4 @@
-package weaver.interfaces.hzy.dms;
+package weaver.interfaces.hzy.dms.resale;
 
 import weaver.conn.RecordSet;
 import weaver.general.BaseBean;
@@ -25,7 +25,7 @@ public class ReSaleSubFlowAction extends BaseBean implements Action {
 
     private String dtFieldChild;//子流程明细字段
 
-
+    private String writeField;//回写字段 若是回写至明细表需标识一下明细表序号 如明细表1则xxx_1
 
 
     @Override
@@ -37,6 +37,8 @@ public class ReSaleSubFlowAction extends BaseBean implements Action {
 
         RequestManager requestManager = requestInfo.getRequestManager();
 
+        String tableName = requestManager.getBillTableName();
+
         Map<String,String> mainTableData = getMainTableData(requestid);
 
         writeLog("主表数据："+mainTableData);
@@ -44,6 +46,8 @@ public class ReSaleSubFlowAction extends BaseBean implements Action {
         WorkflowUtil workflowUtil = new WorkflowUtil();
 
         int billId = requestManager.getBillid();//表单数据id
+
+        List<String> requestids = new ArrayList<>();//触发的子流程
 
         //拆单 to do
         Map<String,List<Map<String,String>>> classificationMap = getDetailMap(billId);
@@ -57,10 +61,10 @@ public class ReSaleSubFlowAction extends BaseBean implements Action {
             mainTableData.put("lcbh",key);
             detail.put("1",mapValue);
             int result = workflowUtil.creatRequest("1","173","HK_销售退货（子流程）",mainTableData,mapValue,"1");
+            requestids.add(String.valueOf(result));
             writeLog("触发成功的子流程请求id：" + result);
         }
-
-
+        writeBackField(tableName,billId,requestids);
 
         return SUCCESS;
     }
@@ -135,5 +139,26 @@ public class ReSaleSubFlowAction extends BaseBean implements Action {
             }
         }
         return classificationMap;
+    }
+
+
+    //将触发的子流程requestid保存到主流程中
+    public void writeBackField(String tableName, int billId, List<String> requestids){
+        writeLog("将触发的子流程requestid保存到主流程中");
+        RecordSet rs = new RecordSet();
+        List<String> field = Util.TokenizerString(writeField, "_");
+        writeLog("field="+field.toString());
+        String sql = "";
+        if (field.size()>1&&!Util.null2String(field.get(1)).equals("")) {
+            tableName = tableName + "_dt" + field.get(1);
+            for (String requestid : requestids){
+                sql = "insert into "+ tableName  + " (mainid," + field.get(0) + ") values (" + billId + "," + requestid + ")" ;
+                rs.executeUpdate(sql);
+            }
+        }else {
+            String requestidStr = String.join(",",requestids);
+            sql = "update "+ tableName  + " set " + field.get(0) + "=? where id = ?"  ;
+            rs.executeUpdate(sql, requestidStr, billId);
+        }
     }
 }
