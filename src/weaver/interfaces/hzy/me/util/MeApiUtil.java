5d71fd4c88e4078a0099ccfb182fa3b1b1d509d2
justin-hzy <771736395@qq.com>
2024-07-25 16:00:38 +0800

    1.MeApiUtil解开被注释的拆卸逻辑 2.优化拆卸同步伯俊其他出入库单接口逻辑

--- src/weaver/interfaces/hzy/me/util/MeApiUtil.java
+++ src/weaver/interfaces/hzy/me/util/MeApiUtil.java
@@ -166,6 +166,8 @@ public class MeApiUtil extends BaseBean {
             params = getSetSonJson(requestId);
         }else if("5".equals(apiId)){
             params = getDismantleFrJon(requestId);
+        }else if("6".equals(apiId)){
+            params = getDismantleSonJon(requestId);
         }
         /*else if("3".equals(apiId)){
             params = getSetFrJson(requestId);
@@ -174,9 +176,7 @@ public class MeApiUtil extends BaseBean {
         }else if("5".equals(apiId)){
             params = getDismantleFrJon(requestId);
         }
-        else if("6".equals(apiId)){
-            params = getDismantleSonJon(requestId);
-        }else if("7".equals(apiId)){
+        else if("7".equals(apiId)){
             params = getTransCodeFrJson(requestId);
         }else if("8".equals(apiId)){
             params = getTransCodeSonJson(requestId);
@@ -426,7 +426,7 @@ public class MeApiUtil extends BaseBean {
     }
 
     public List<String> getSetFrJson(String requestId){
-        writeLog("---------开始组装ME接口组套-父项出库单参数-----------");
+        writeLog("---------开始组装ME接口组套-父项入库单参数-----------");
         List<String> params = new ArrayList<>();
         RecordSet rs1 = new RecordSet();
 
@@ -525,34 +525,45 @@ public class MeApiUtil extends BaseBean {
         List<String> params = new ArrayList<>();
         RecordSet rs1 = new RecordSet();
 
-        String sql1 = "select dt3.spbm,main.shdc1,dt3.sjrksl from formtable_main_242 as main inner join formtable_main_242_dt3 dt3 on main.id = dt3.mainid where dt3.sjrksl is not null and dt3.sjcksl is null and main.requestId = ?";
+        String sql1 = "select dt3.spbm,main.shdc1,dt3.sjrksl,main.lcbh from formtable_main_242 as main inner join formtable_main_242_dt3 dt3 on main.id = dt3.mainid where  main.requestId = ?";
 
         writeLog(sql1+","+requestId);
         rs1.executeQuery(sql1, requestId);
 
+        JSONObject jsonObject = new JSONObject();
+        JSONArray subOthers = new JSONArray();
+
         while (rs1.next()) {
-            JSONObject jsonObject = new JSONObject();
+
 
             String shdc1 = Util.null2String(rs1.getString("shdc1")); //子项-入库仓库
             String spbm = Util.null2String(rs1.getString("spbm")); //物料编码
             String sjrksl = Util.null2String(rs1.getString("sjrksl")); //子项-出库数量
+            String lcbh = Util.null2String(rs1.getString("lcbh")); //流程编号
 
             jsonObject.put("cstore",shdc1);
-            jsonObject.put("sku",spbm);
-            jsonObject.put("qty",sjrksl);
+
+            JSONObject subOther = new JSONObject();
+            subOther.put("sku",spbm);
+            subOther.put("qty",sjrksl);
+            subOthers.add(subOther);
 
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
             String now = dateFormat.format(new Date());
             jsonObject.put("billDate",now);
-            jsonObject.put("description","拆卸流程-入库单");
+            jsonObject.put("description","拆卸流程-入库单-"+lcbh);
             jsonObject.put("requestId",requestId);
 
-            String param = jsonObject.toJSONString();
-
-            writeLog("param="+param);
-
-            params.add(param);
         }
+
+        jsonObject.put("subOthers",subOthers);
+
+        String param = jsonObject.toJSONString();
+
+        writeLog("param="+param);
+
+        params.add(param);
+
         writeLog("params="+params.toString());
         return params;
     }
@@ -563,7 +574,7 @@ public class MeApiUtil extends BaseBean {
         List<String> params = new ArrayList<>();
         RecordSet rs1 = new RecordSet();
 
-        String sql1 = "select main.lcbh,dt3.spbm,main.fhdc1,dt3.sjcksl from formtable_main_242 as main inner join formtable_main_242_dt3 dt3 on main.id = dt3.mainid where dt3.sjcksl is not null and dt3.sjrksl is null and main.requestId = ?";
+        String sql1 = "select distinct dt1.fxwlbm,main.fhdc1,dt1.sjztfxsl,main.lcbh from formtable_main_242 as main inner join formtable_main_242_dt1 dt1 on main.id = dt1.mainid where main.requestId = ?";
         writeLog(sql1+","+requestId);
         rs1.executeQuery(sql1, requestId);
 
@@ -575,16 +586,16 @@ public class MeApiUtil extends BaseBean {
 
 
             String fhdc1 = Util.null2String(rs1.getString("fhdc1")); //出库仓库
-            String spbm = Util.null2String(rs1.getString("spbm")); //物料编码
-            String sjcksl = Util.null2String(rs1.getString("sjcksl")); //父项实际入库数量
+            String fxwlbm = Util.null2String(rs1.getString("fxwlbm")); //物料编码
+            String sjztfxsl = Util.null2String(rs1.getString("sjztfxsl")); //父项实际出库数量
             String lcbh = Util.null2String(rs1.getString("lcbh")); //流程编号
 
 
             jsonObject.put("cstore",fhdc1);
 
             JSONObject subOther = new JSONObject();
-            subOther.put("sku",spbm);
-            subOther.put("qty",sjcksl);
+            subOther.put("sku",fxwlbm);
+            subOther.put("qty",sjztfxsl);
             subOthers.add(subOther);
 
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
