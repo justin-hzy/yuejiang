207594ddf0e1382aec4b7b938ca9dd9ea1304d56
justin-hzy <771736395@qq.com>
2024-07-25 18:00:19 +0800

    1.新增TransCodeAction、TransCodeService

---
+++ src/weaver/interfaces/hzy/k3/transcode/action/TransCodeAction.java
@@ -0,0 +1,15 @@
+package weaver.interfaces.hzy.k3.transcode.action;
+
+import com.weaver.general.BaseBean;
+import weaver.interfaces.workflow.action.Action;
+import weaver.soa.workflow.request.RequestInfo;
+
+public class TransCodeAction extends BaseBean implements Action {
+    @Override
+    public String execute(RequestInfo requestInfo) {
+
+
+
+        return null;
+    }
+}
---
+++ src/weaver/interfaces/hzy/k3/transcode/service/TransCodeService.java
@@ -0,0 +1,6 @@
+package weaver.interfaces.hzy.k3.transcode.service;
+
+import com.weaver.general.BaseBean;
+
+public class TransCodeService extends BaseBean {
+}
