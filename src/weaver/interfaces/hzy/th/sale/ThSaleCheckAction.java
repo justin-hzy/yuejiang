10ba125b09bc6bc84d752d0a9ec3318f1e4c0be3
justin-hzy <771736395@qq.com>
2024-12-31 12:27:32 +0800

    泰国销售下单流程增加销售单重复校验功能

---
+++ src/weaver/interfaces/hzy/th/sale/ThSaleCheckAction.java
@@ -0,0 +1,72 @@
+package weaver.interfaces.hzy.th.sale;
+
+import weaver.conn.RecordSet;
+import weaver.general.BaseBean;
+import weaver.interfaces.workflow.action.Action;
+import weaver.soa.workflow.request.RequestInfo;
+
+import java.util.*;
+
+public class ThSaleCheckAction extends BaseBean implements Action {
+    @Override
+    public String execute(RequestInfo requestInfo) {
+
+        writeLog("开始执行ThSaleCheckAction");
+
+        writeLog("开始执行CheckSaleAction");
+
+        List<Map<String, String>> detailDatas1 = weaver.interfaces.tx.util.WorkflowToolMethods.getDetailTableInfo(requestInfo, 1);
+
+        String requestId = requestInfo.getRequestid();
+
+        writeLog("detailDatas=" + detailDatas1.toString());
+
+        List<String> orderNos = new ArrayList<>();
+
+        if (detailDatas1.size() > 0) {
+            Set<String> hashSet = new HashSet<>();
+
+            for (Map<String, String> detailData : detailDatas1) {
+                String khddh = detailData.get("erp_order");
+                if (!hashSet.contains(khddh)){
+                    hashSet.add(khddh);
+                }
+            }
+
+            writeLog("hashSet="+hashSet.toString());
+
+            for (String orderNo : hashSet){
+                String sql = "select * from formtable_main_347 main where erp_order = ?";
+                RecordSet dtRs = new RecordSet();
+                dtRs.executeQuery(sql, orderNo);
+
+                if(dtRs.next()){
+                    orderNos.add(orderNo);
+                }
+            }
+            writeLog("orderNos="+orderNos.toString());
+        }
+
+        if(orderNos.size()>0){
+            String cause = "";
+
+            for (String orderNo :  orderNos){
+                cause = cause + orderNo+",";
+            }
+
+            cause = cause + "重复订单";
+
+            writeLog("cause="+cause);
+
+            String updateSql = "update formtable_main_350 set cause = ? , isRpt = ? where requestid = ?";
+            RecordSet dtRs = new RecordSet();
+            dtRs.executeUpdate(updateSql,cause,0,requestId);
+        }else {
+            String updateSql = "update formtable_main_350 set cause = '' ,isRpt = ? where requestid = ?";
+            RecordSet dtRs = new RecordSet();
+            dtRs.executeUpdate(updateSql,1,requestId);
+        }
+
+        return SUCCESS;
+    }
+}
